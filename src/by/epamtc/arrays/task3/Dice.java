641fb77e134962881f1f102d6aa3fbb472c476e2
Viktoria Shamuradova <viktoriashamuradova@gmail.com>
2020-06-17 14:01:14 +0300

    super lock

--- src/by/epamtc/arrays/task3/Dice.java
+++ src/by/epamtc/arrays/task3/Dice.java
@@ -1,6 +1,8 @@
 package by.epamtc.arrays.task3;
 
 
+import java.util.Objects;
+
 public class Dice {
     private int frontFaceValue;
 
@@ -9,12 +11,33 @@ public class Dice {
         this.frontFaceValue = frontFaceValue;
     }
 
+
+
     public void setFrontFaceValue(int frontFaceValue) {
         this.frontFaceValue = frontFaceValue;
     }
 
     public int getFrontFaceValue() {
         return frontFaceValue;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dice dice = (Dice) o;
+        return frontFaceValue == dice.frontFaceValue;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(frontFaceValue);
+    }
+
+    @Override
+    public String toString() {
+        return "Dice{" +
+                "frontFaceValue=" + frontFaceValue +
+                '}';
     }
 }
 
